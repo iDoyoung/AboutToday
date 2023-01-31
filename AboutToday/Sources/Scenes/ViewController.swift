b2ff03d633ed016ba78d7b910cb923b95b3e6f72
iDoyoung <ido.young@icloud.com>
2023-01-31 09:16:16 +0900

    Initial commit :D

---
+++ AboutToday/Sources/Scenes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AboutToday
+//
+//  Created by Doyoung on 2023/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    deinit {
+        #if DEBUG
+        print("Deinit View Controller")
+        #endif
+    }
+}
+
