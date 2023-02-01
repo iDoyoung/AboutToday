0b97324272a2e6157c5e9813f517d10c65e826a7
iDoyoung <ido.young@icloud.com>
2023-02-01 09:26:27 +0900

    Add Weather Model

---
+++ AboutToday/Sources/Models/Weather.swift
@@ -0,0 +1,64 @@
+//
+//  Weather.swift
+//  AboutToday
+//
+//  Created by Doyoung on 2023/02/01.
+//
+
+import Foundation
+
+struct Weather: Decodable {
+    
+    let id: Int
+    let city: String
+    let main: Main
+    let weahter: [Weather]
+    let wind: Wind
+    let coordinate: Coordinate
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case city = "name"
+        case main
+        case weahter
+        case wind
+        case coordinate = "coord"
+    }
+    
+    struct Main: Codable {
+        let temp: Double
+        let feels: Double
+        let minTemp: Double
+        let maxTemp: Double
+        let pressure: Int
+        let humidity: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case temp
+            case humidity
+            case feels = "feels_like"
+            case minTemp = "temp_min"
+            case maxTemp = "temp_max"
+            case pressure
+        }
+    }
+    
+    struct Weather: Codable {
+        let description: String
+        let icon: String
+    }
+    
+    struct Wind: Codable {
+        let speed: Double
+    }
+    
+    struct Coordinate: Codable {
+        let longitude: Double
+        let latitude: Double
+        
+        enum CodingKeys: String, CodingKey {
+            case longitude = "lon"
+            case latitude = "lat"
+        }
+    }
+}
