0d7ddcef58a314cb9a8914bae97d7450172d55d1
iDoyoung <ido.young@icloud.com>
2023-02-03 13:17:42 +0900

    Add Navigation Left Bar Button Item

--- AboutToday/Sources/Scenes/Today/TodayViewController.swift
+++ AboutToday/Sources/Scenes/Today/TodayViewController.swift
@@ -7,9 +7,32 @@
 
 import UIKit
 
-final class TodayViewController: ViewController {
+protocol TodayDisplayLogic: AnyObject {
+}
 
+final class TodayViewController: ViewController, TodayDisplayLogic {
+    
+    lazy var navigationLeftBarButtonItem: UIBarButtonItem = {
+        let barbuttionItem = UIBarButtonItem(image: UIImage(systemName: "questionmark"), style: .plain, target: self, action: nil)
+        barbuttionItem.accessibilityLabel = "Today weather"
+        return barbuttionItem
+    }()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .systemBackground
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.leftBarButtonItem = navigationLeftBarButtonItem
+    }
+    
+    func displayNavigationBarLeftItem(weather image: UIImage) {
+        navigationLeftBarButtonItem.image = image
     }
 }
