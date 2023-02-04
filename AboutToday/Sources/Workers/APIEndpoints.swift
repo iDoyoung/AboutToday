c44f454b581d6ff1035b341133f902d505ddc8f5
iDoyoung <ido.young@icloud.com>
2023-02-04 19:36:58 +0900

    Add endpoint to get weather icon

--- AboutToday/Sources/Workers/APIEndpoints.swift
+++ AboutToday/Sources/Workers/APIEndpoints.swift
@@ -10,6 +10,10 @@ import Foundation
 struct APIEndpoints {
     
     static func getCurrentLocation(latitude: String, longitude: String) -> Endpoint<Weather> {
-        return Endpoint(path: "weather?", queryParameters: ["lat": latitude, "lon": longitude])
+        return Endpoint(path: "data/2.5/weather?", queryParameters: ["lat": latitude, "lon": longitude])
+    }
+    
+    static func getWeatherIcon(with imagePath: String) -> Endpoint<Data> {
+        return Endpoint(path: "img/wn/\(imagePath)@2x.png")
     }
 }
