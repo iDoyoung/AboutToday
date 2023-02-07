3b22ee5db04a129fb0051e4c7cbca10a9dc9108f
iDoyoung <ido.young@icloud.com>
2023-02-07 15:40:42 +0900

    Add UI Properties of Weather View

--- AboutToday/Sources/Scenes/Today/TodayViewController.swift
+++ AboutToday/Sources/Scenes/Today/TodayViewController.swift
@@ -15,10 +15,45 @@ final class TodayViewController: ViewController, TodayDisplayLogic {
     
     var interactor: TodayBusinessLogic?
     
-    lazy var navigationLeftBarButtonItem: UIBarButtonItem = {
-        let barbuttionItem = UIBarButtonItem(image: nil, style: .plain, target: self, action: nil)
-        barbuttionItem.accessibilityLabel = "Today weather"
-        return barbuttionItem
+    //MARK: UI Properties
+    private lazy var navigationLeftBarButtonItem: UIBarButtonItem = {
+        let barButtionItem = UIBarButtonItem(image: nil, style: .plain, target: self, action: nil)
+        barButtionItem.accessibilityLabel = "Today weather"
+        return barButtionItem
+    }()
+    
+    private let weatherView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let locationView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let cityLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
+    }()
+    
+    private let currentTempLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .largeTitle)
+        return label
+    }()
+    
+    private let maxTempLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .caption2)
+        return label
+    }()
+    
+    private let minTempLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .caption2)
+        return label
     }()
     
     override func loadView() {
@@ -44,10 +79,31 @@ final class TodayViewController: ViewController, TodayDisplayLogic {
     func displayWeather(viewModel: TodayWeather.Fetched.ViewModel) {
         assert(Thread.isMainThread, "Thread is not main")
         setLeftBarButtonItemImage(viewModel.image)
+        setCityLabel(text: viewModel.city)
+        setCurrentTempLabel(text: viewModel.currentTemperature)
+        setMaxTempLabel(text: viewModel.maxTemperature)
+        setMinTempLabel(text: viewModel.minTemperature)
     }
     
     private func setLeftBarButtonItemImage(_ image: UIImage?) {
         assert(image != nil, "Image is nil")
         navigationLeftBarButtonItem.image = image
     }
+    
+    private func setCityLabel(text: String) {
+        cityLabel.text = text
+    }
+    
+    private func setCurrentTempLabel(text: String) {
+        currentTempLabel.text = text
+    }
+    
+    private func setMaxTempLabel(text: String) {
+        maxTempLabel.text = text
+    }
+    
+    private func setMinTempLabel(text: String) {
+        minTempLabel.text = text
+    }
+    
 }
