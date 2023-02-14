86ce49636aa0599f745a97c72c0c786aa0ba24de
iDoyoung <ido.young@icloud.com>
2023-02-14 22:06:10 +0900

    Add Weather Detail Presenter

---
+++ AboutToday/Sources/Scenes/Weather Detail/WeatherDetailPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherDetailPresenter.swift
+//  AboutToday
+//
+//  Created by Doyoung on 2023/02/14.
+//
+
+import UIKit
+
+protocol WeatherDetailPresentLogic {}
+
+final class WeatherDetailPresenter {
+    
+    weak var viewController: WeatherDetailDisplayLogic?
+}
