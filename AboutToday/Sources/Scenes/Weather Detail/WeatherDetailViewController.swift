45e0f38c3c0a9487741974f6dd224f729583d5b3
iDoyoung <ido.young@icloud.com>
2023-02-14 21:32:22 +0900

    Add Weather Detail View

---
+++ AboutToday/Sources/Scenes/Weather Detail/WeatherDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherDetailViewController.swift
+//  AboutToday
+//
+//  Created by Doyoung on 2023/02/14.
+//
+
+import UIKit
+
+protocol WeatherDetailDisplayLogic: AnyObject {
+}
+    
+final class WeatherDetailViewController: ViewController, WeatherDetailDisplayLogic {
+
+//    var interactor: WeatherDetailBusinessLogic?
+    //MARK: - UI Properties
+    var contentView = WeatherDetailView()
+    
+    override func loadView() {
+        view = contentView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
