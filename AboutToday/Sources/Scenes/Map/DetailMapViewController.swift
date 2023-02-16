93ecbc590cdc14168356b3ee167280a963be4960
iDoyoung <ido.young@icloud.com>
2023-02-17 08:47:53 +0900

    Implement Creating Annotation View To Display Annotation

--- AboutToday/Sources/Scenes/Map/DetailMapViewController.swift
+++ AboutToday/Sources/Scenes/Map/DetailMapViewController.swift
@@ -9,21 +9,55 @@ import UIKit
 import MapKit
 
 protocol DetailMapDisplayLogic: AnyObject {
+    func displayAnnotationOfLocation(viewModel: PhotosLocation.Loaded.ViewModel)
 }
 
 final class DetailMapViewController: ViewController, DetailMapDisplayLogic {
-
-    let contetnView = DetailMapView()
+    
+    let contentView = DetailMapView()
     var interactor: DetailMapBusinessLogic?
     var router: (DetailMapRouting&DetailMapDataPassing)?
     
+    private let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.showsUserLocation = true
+        return mapView
+    }()
+    
     //MARK: - Life Cycle
     override func loadView() {
-        view = contetnView
+        view = mapView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerMapAnnotationViews()
+        mapView.delegate = self
         interactor?.loadPhotoLocations()
+    }
+    
+    /// - Tag: Register annotation views
+    private func registerMapAnnotationViews() {
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: PhotoAnnotation.reuseIdentifier)
+    }
+    
+    //MARK: - Display Logics
+    func displayAnnotationOfLocation(viewModel: PhotosLocation.Loaded.ViewModel) {
+        mapView.addAnnotations(viewModel.annotations)
+    }
+}
+
+extension DetailMapViewController: MKMapViewDelegate {
+    
+    /// - Tag: Create Anotataion View
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let view = mapView.dequeueReusableAnnotationView(withIdentifier: PhotoAnnotation.reuseIdentifier, for: annotation)
+        
+        guard !(annotation is MKUserLocation) else { return nil }
+        if let markerAnnotationView = view as? MKMarkerAnnotationView {
+            markerAnnotationView.markerTintColor = .systemYellow
+            markerAnnotationView.glyphImage = UIImage(systemName: "camera")
+        }
+        return view
     }
 }
