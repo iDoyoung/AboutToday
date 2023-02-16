dfe30241a1d91e9de98efa2522febff09a96b8c9
iDoyoung <ido.young@icloud.com>
2023-02-16 10:10:57 +0900

    Add Detail Map Presenter

---
+++ AboutToday/Sources/Scenes/Map/DetailMapPresenter.swift
@@ -0,0 +1,21 @@
+//
+//  DetailMapPresenter.swift
+//  AboutToday
+//
+//  Created by Doyoung on 2023/02/16.
+//
+
+import UIKit
+
+protocol DetailMapPresenting {
+    func presentPhotosLocations()
+}
+
+final class DetailMapPresenter: DetailMapPresenting {
+    
+    weak var viewController: DetailMapDisplayLogic?
+    
+    func presentPhotosLocations() {
+        <#code#>
+    }
+}
