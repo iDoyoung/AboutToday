956a642892ed1fa639fcb7ecdfdb641b3d5a91ac
iDoyoung <ido.young@icloud.com>
2023-02-21 15:10:13 +0900

    Implement Load Current Location

--- AboutToday/Sources/Scenes/Map/DetailMapnteractor.swift
+++ AboutToday/Sources/Scenes/Map/DetailMapnteractor.swift
@@ -6,20 +6,30 @@
 //
 
 import Foundation
+import CoreLocation
 import Photos
 
 protocol DetailMapBusinessLogic {
     func loadPhotoLocations()
+    func loadCurrentLocation()
 }
 
 protocol DetailMapDataStore {
+    var currentLocation: CLLocation? { get set }
     var assets: PHFetchResult<PHAsset>? { get set }
 }
 
 final class DetailMapInteractor: DetailMapBusinessLogic, DetailMapDataStore {
     
     var presenter: DetailMapPresenting?
+    //MARK: Data Store
     var assets: PHFetchResult<PHAsset>?
+    var currentLocation: CLLocation?
+    
+    func loadCurrentLocation() {
+        guard let currentLocation else { return }
+        presenter?.presentCurrentLocation(currentLocation)
+    }
     
     func loadPhotoLocations() {
         var locations = [CLLocation]()
--- AboutTodayTests/Scenes/DetailMap/DetailMapInteractor.swift
+++ AboutTodayTests/Scenes/DetailMap/DetailMapInteractor.swift
@@ -7,6 +7,8 @@
 
 import XCTest
 import Photos
+import CoreLocation
+
 @testable import AboutToday
 
 final class DetailMapInteractorTests: XCTestCase {
@@ -31,9 +33,14 @@ final class DetailMapInteractorTests: XCTestCase {
     final class DetailMapPresenterSpy: DetailMapPresenting {
         
         var presentPhotosLocationCalled = false
+        var presentCurrentLocationCalled = false
         
         func presentPhotosLocations(response: AboutToday.PhotosLocation.Loaded.Response) {
             presentPhotosLocationCalled = true
+        }
+        
+        func presentCurrentLocation(_ location: CLLocation) {
+            presentCurrentLocationCalled = true
         }
     }
     
@@ -42,5 +49,11 @@ final class DetailMapInteractorTests: XCTestCase {
         sut.assets = PHFetchResult<PHAsset>()
         sut.loadPhotoLocations()
         XCTAssert(detailMapPresenterSpy.presentPhotosLocationCalled)
+    }
+    
+    func test_loadCurrentLocation_shouldCallLoadCurrentLocation_whenBeGiveCurrentLocation() {
+        sut.currentLocation = CLLocation(latitude: 0.1, longitude: 0.1)
+        sut.loadCurrentLocation()
+        XCTAssert(detailMapPresenterSpy.presentCurrentLocationCalled)
     }
 }
